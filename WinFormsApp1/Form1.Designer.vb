f268ec6f13d69116b60edf35f3acdc19b4543ffe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-03 10:47:10 +0900

    フォームをデザイン

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -22,10 +22,62 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.picView = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        CType(Me.picView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picView
+        '
+        Me.picView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.picView.BackColor = System.Drawing.Color.White
+        Me.picView.Location = New System.Drawing.Point(10, 10)
+        Me.picView.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.picView.Name = "picView"
+        Me.picView.Size = New System.Drawing.Size(665, 338)
+        Me.picView.TabIndex = 0
+        Me.picView.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 371)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(581, 19)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnDraw.Location = New System.Drawing.Point(599, 367)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(75, 23)
+        Me.btnDraw.TabIndex = 2
+        Me.btnDraw.Text = "Button1"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(686, 402)
+        Me.Controls.Add(Me.btnDraw)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.picView)
+        Me.Font = New System.Drawing.Font("ＭＳ ゴシック", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.picView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents picView As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnDraw As Button
 End Class
