37c36010d79829b5541e35526a916c2f48ffc114
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-03 19:14:12 +0900

    描画処理を変更

--- WinFormsApp1/View.vb
+++ WinFormsApp1/View.vb
@@ -3,6 +3,7 @@
     Public Structure ViewInfo
         Public gView As System.Drawing.Graphics
         Public imgCanvas As System.Drawing.Bitmap
+        Public imgCell As System.Drawing.Bitmap
         Public imgIcons As System.Drawing.Bitmap
     End Structure
 
@@ -67,7 +68,9 @@
                 w = .Width
                 h = .Height
             End With
+
             .imgCanvas = New System.Drawing.Bitmap(w, h)
+            .imgCell = New System.Drawing.Bitmap(128, 32)
         End With
 
     End Sub
@@ -97,43 +100,62 @@
     Public Sub DrawTextOn(ByRef utViewInfo As ViewInfo, ByVal strText As String, ByRef fnt As Font)
 
         Dim gCanvas As System.Drawing.Graphics
-        Dim gIcons As System.Drawing.Graphics
+        Dim gCell As System.Drawing.Graphics
+        ' Dim gIcons As System.Drawing.Graphics
         Dim destX As Integer, destY As Integer
         Dim copyWidth As Integer, copyHeight As Integer
         Dim srcX As Integer, srcY As Integer
-        Dim hdcSrc As IntPtr, hdcDst As IntPtr, hobjOld As IntPtr
+        Dim hdcSrc As IntPtr, hdcDst As IntPtr, hdcCell As IntPtr
+        Dim hOldObj As IntPtr
+        Dim srcRect As System.Drawing.Rectangle
 
         With utViewInfo
             gCanvas = System.Drawing.Graphics.FromImage(.imgCanvas)
-            gIcons = System.Drawing.Graphics.FromImage(.imgIcons)
+            gCell = System.Drawing.Graphics.FromImage(.imgCell)
+            ' gIcons = System.Drawing.Graphics.FromImage(.imgIcons)
 
             gCanvas.FillRectangle(Brushes.LightGray, gCanvas.VisibleClipBounds)
 
+            gCell.FillRectangle(Brushes.White, gCell.VisibleClipBounds)
+
+            Dim p As New System.Drawing.Pen(Color.Black, 1)
+            gCell.DrawRectangle(p, 0, 0, .imgCell.Width - 1, .imgCell.Height - 1)
+
             ' アイコンを転送する
-            destX = 32
-            destY = 32
+            destX = 8
+            destY = 8
             copyWidth = 16
             copyHeight = 16
             srcX = 16
             srcY = 0
 
             hdcDst = gCanvas.GetHdc()
+            hdcCell = gCell.GetHdc()
+
             hdcSrc = CreateCompatibleDC(hdcDst)
-            hobjOld = SelectObject(hdcSrc, .imgIcons.GetHbitmap())
-            BitBlt(hdcDst, destX, destY, copyWidth, copyHeight, hdcSrc, srcX, srcY + 16, SRCAND)
-            BitBlt(hdcDst, destX, destY, copyWidth, copyHeight, hdcSrc, srcX, srcY, SRCPAINT)
+            hOldObj = SelectObject(hdcSrc, .imgIcons.GetHbitmap())
+            BitBlt(hdcCell, destX, destY, copyWidth, copyHeight, hdcSrc, srcX, srcY + 16, SRCAND)
+            BitBlt(hdcCell, destX, destY, copyWidth, copyHeight, hdcSrc, srcX, srcY, SRCPAINT)
+            SelectObject(hdcSrc, hOldObj)
 
             DeleteDC(hdcSrc)
-            gCanvas.ReleaseHdc(hdcDst)
+            ' gIcons.Dispose()
 
-            gCanvas.DrawString(strText, fnt, Brushes.Red, destX + 32, destY)
+            ' セルの内容を書き終えたらキャンバスに転送する
+            gCell.ReleaseHdc(hdcCell)
+            gCell.DrawString(strText, fnt, Brushes.Red, destX + 32, destY)
+            gCell.Dispose()
+
+            srcRect = New System.Drawing.Rectangle(0, 0, 128, 32)
+            'BitBlt(hdcDst, 32, 32, 128, 32, hdcCell, 0, 0, SRCCOPY)
+            gCanvas.ReleaseHdc(hdcDst)
+            gCanvas.DrawImage(.imgCell, 32, 32, srcRect, GraphicsUnit.Pixel)
 
             ' 最後に表示用のピクチャーボックスに転送する
             .gView.DrawImage(.imgCanvas, 0, 0)
         End With
 
         gCanvas.Dispose()
-        gIcons.Dispose()
 
     End Sub
 
