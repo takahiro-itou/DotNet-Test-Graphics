36f4cece7402842ac3065ccc8deeacf0b20c8f31
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-11-03 11:09:34 +0900

    イベントハンドラを実装

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
 
+    Private utViewInfo As ViewInfo
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadResources("Resource", utViewInfo)
+        SetupGraphics(Me.picView, utViewInfo)
+    End Sub
+
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+        DrawTextOn(utViewInfo, Me.TextBox1.Text)
+    End Sub
 End Class
